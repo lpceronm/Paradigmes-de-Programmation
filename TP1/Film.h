e8452732f3bed7fb8befa7c8873823bc36b7173f
Laura Ceron <lpceronm@unal.edu.co>
2018-10-02 23:01:46 +0200

    fix film

--- TP1/Film.h
+++ TP1/Film.h
@@ -38,7 +38,6 @@ public:
   void show(ostream &s){
     Video::show(s);
     s << "Chapters duration: " << "\n";
-    
     for(int i = 0; i < chapNumber; i++){
       s <<"\t Chapter" << i << ": " << chapters[i] << "\n";
     }
