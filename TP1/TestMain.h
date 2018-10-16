e9bcc08b585342aa162ae1e0affacdf8c5d7d943
Laura Ceron <lpceronm@unal.edu.co>
2018-10-17 01:04:45 +0200

    Delete from tables

--- TP1/TestMain.h
+++ TP1/TestMain.h
@@ -15,9 +15,9 @@ using namespace std;
 using Sst = std::shared_ptr<Store>;
 
 void testMain(){
-  int *chap3 = new int[12]{1, 2, 3, 4,5,6,7,8,9,10,11,12};
+  int *chap3 = new int[12]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
   int *chap2 = new int[3]{1, 2, 3};
-  Film * f3 = new Film("film", "path", 20, 12, chap3);
+  Film *f3 = new Film("film", "path", 20, 12, chap3);
 
   // Film * f1 = new Film("film", "path", 20, 3, chap2);
   // f1->show(cout);
@@ -33,7 +33,6 @@ void testMain(){
 
   // f1->show(cout);
 
-
   Sst store(new Store());
 
   Group *list = new Group("name");
@@ -45,41 +44,41 @@ void testMain(){
 
   Smt v1(new Video("Video", "video/test.png", 11));
   Smt v2(new Video("Video2", "video/test.png", 11));
-  int *chap = new int[12]{1, 2, 3, 4,5,6,7,8,9,10,11,12};
+  int *chap = new int[12]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
   Smt f(new Film("film", "path", 20, 12, chap));
 
-  
   Smt p = store->createPhoto("PhotoMap", "photo/test.png", 1, 1 + 2.0);
-  
-  Sgr gr  = store->createGroup("groupname");
+
+  Sgr gr = store->createGroup("groupname");
   gr->push_back(p1);
   gr->push_back(p);
+  p.reset();
 
- 
   gr->show(cout);
+  gr.reset();
 
   store->showElement("Name", cout);
   store->showElement("groupname", cout);
   store->showElement("PhotoMap", cout);
-  store->playElement("Photo");
-  store->playElement("Name");
-
-
-
+  // store->playElement("PhotoMap");
+  // store->playElement("Name");
+  store->deleteElement("PhotoMap");
+  store->deleteGroup("groupname");
 
   // delete[] chap;
 
-  // list->push_back(p1);
-  // list->push_back(v1);
-  // // list->push_back(f);
-  // list->push_back(p3);
-  // // other_list.push_back(p1);
-  // // other_list.push_back(p2);
-  // // other_list.push_back(f);
-  // // other_list.push_back(v2);
+  cout << "**************** Something different" << endl;
+  list->push_back(p1);
+  list->push_back(v1);
+  list->push_back(f);
+  list->push_back(p3);
+  other_list.push_back(p1);
+  other_list.push_back(p2);
+  other_list.push_back(f);
+  other_list.push_back(v2);
 
-  // // list->show(cout);
-  // // other_list.show(cout);
+  list->show(cout);
+  other_list.show(cout);
 
   // cout << "****** DELETED Map group \n";
 
